bedf57f01f0bb68d8091e77e93849963403af8f9
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-11-27 11:24:33 +0000

    add missing RedisModule import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { RedisModule } from "../redis/redis.module";
 import { UsersRepository } from "./users.repository";
 import { UsersService } from "./users.service";
 
 @Module({
+    imports: [RedisModule],
     providers: [UsersRepository, UsersService],
     exports: [UsersService],
 })
